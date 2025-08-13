f246f3e5945e60103d7c88ffc73527bcd6d5422b
vinhLuu <vinhluucongk17@gmail.com>
2025-08-13 08:56:36 +0700

    Add ArgumentParser

--- RDN/test_RDN.py
+++ RDN/test_RDN.py
@@ -5,27 +5,46 @@ import PIL
 from PIL import Image
 from torchvision.transforms import ToTensor, Resize
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("--image_path", type=str, default="", help="Path to the input image")
+parser.add_argument("--scale_factor", type=int, default = 2, help="Scale factor for super-resolution")
 
 if __name__ == "__main__":
-   model = RDN(scale_factor=2, num_channels=3, num_features=64, growth_rate=64, num_blocks=16,num_layers=8)
-   model.load_state_dict(torch.load(r'D:\học\Tài Liệu CNTT\image_super_resolution\weights\image_64_128_250f\rdn_x2-C8-D16-G064-G64-best_psnr26_7808.pth'))
+   args = parser.parse_args()
+   path = args.image_path # Path to the input image
+   scale_factor = args.scale_factor # Scale-factor
+   if not path:
+      print("No image path provided.")
+      exit(0)
+   if torch.cuda.is_available():
+      device = torch.device('cuda:0')
+      print(f"Using GPU: {torch.cuda.get_device_name(0)}")
+   else:
+      device = torch.device('cpu')
+      print("No GPU available, using CPU instead.")
+      
+   # Initialize model
+   model = RDN(scale_factor=scale_factor, num_channels=3, num_features=64, growth_rate=64, num_blocks=16,num_layers=8)
+   model.to(device) # Put model to GPU
+   model.load_state_dict(torch.load(r"D:\học\Tài Liệu CNTT\image_super_resolution\weights\image_64_128_250f\rdn_x2-C8-D16-G064-G64-best_psnr26_9196_13.pth"))
    model.eval()  # Set the model to evaluation mode
 
-   path = r"D:\học\Tài Liệu CNTT\image_super_resolution\dataset\imagnet_256_full_origin\zucchini\506.jpg"
-   image = Image.open(path).convert("RGB")
-   # print(f"{np.array(image)}\n--------------------")
-   image1 = Resize((512, 512), interpolation=PIL.Image.LANCZOS)(image)
-   
+   image = Image.open(path).convert("RGB") # Open Image from path
+   new_size = (image.width * scale_factor, image.height * scale_factor)
+   image1 = Resize(new_size, interpolation = PIL.Image.BICUBIC)(image)
    image1.show()
    
+   # To tensor from image
    image_tensor = ToTensor()(image).unsqueeze(0)  # Add batch dimension
-   # image_tensor = pil_to_tensor_255(image).unsqueeze(0) # Add batch dimension
-
+   if torch.cuda.is_available():
+      image_tensor = image_tensor.to(device)
    with torch.no_grad():
       output = model(image_tensor)
    output_image = output.squeeze(0)  # Remove batch dimension
    output_image = output_image.permute(1, 2, 0)  # Change to HWC format
-   output_image = (output_image * 255).clamp(0, 255).byte().numpy()  # Convert to uint8
+   output_image = (output_image.cpu() * 255).clamp(0, 255).byte().numpy()  # Convert to uint8
    # print(output_image)
    output_image = Image.fromarray(output_image)
    output_image.show()
