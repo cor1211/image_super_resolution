d7062559126483acc3b8d2d5fa9b096f9daadc2d
vinhLuu <vinhluucongk17@gmail.com>
2025-08-14 15:37:29 +0700

    Add auto save HR_image function

--- RDN/test_RDN.py
+++ RDN/test_RDN.py
@@ -8,13 +8,20 @@ import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("--image_path", type=str, default="", help="Path to the input image")
+parser.add_argument("--image_path", type=str, default="../data/sample/baboon.png", help="Path to the input image")
 parser.add_argument("--scale_factor", type=int, default = 2, help="Scale factor for super-resolution")
+parser.add_argument("--weight_path", type = str, default = "../weights/image_64_128_250f/rdn_x2-C8-D16-G064-G64-best_psnr26_9196_13.pth", help = "Path to the model weights")
+parser.add_argument("--save", type = bool, default=True, help="Save HR-image")
+parser.add_argument("--save_path", type=str, default="../data/sample/", help="Path to save image")
 
 if __name__ == "__main__":
    args = parser.parse_args()
    path = args.image_path # Path to the input image
    scale_factor = args.scale_factor # Scale-factor
+   weight_path = args.weight_path # Path to the model weights
+   save = args.save # Save HR-image
+   save_path = args.save_path # Path to save image
+   
    if not path:
       print("No image path provided.")
       exit(0)
@@ -28,7 +35,7 @@ if __name__ == "__main__":
    # Initialize model
    model = RDN(scale_factor=scale_factor, num_channels=3, num_features=64, growth_rate=64, num_blocks=16,num_layers=8)
    model.to(device) # Put model to GPU
-   model.load_state_dict(torch.load(r"D:\học\Tài Liệu CNTT\image_super_resolution\weights\image_64_128_250f\rdn_x2-C8-D16-G064-G64-best_psnr26_9196_13.pth"))
+   model.load_state_dict(torch.load(weight_path)) # Load weight pretrained
    model.eval()  # Set the model to evaluation mode
 
    image = Image.open(path).convert("RGB") # Open Image from path
@@ -48,3 +55,9 @@ if __name__ == "__main__":
    # print(output_image)
    output_image = Image.fromarray(output_image)
    output_image.show()
+   if save:
+      hr_img_path = save_path + path.split('/')[-1].split('.')[0] + ".tiff"
+      output_image.save(hr_img_path)
+      print(f"HR of {path} saved in {hr_img_path}")
+      
+   
